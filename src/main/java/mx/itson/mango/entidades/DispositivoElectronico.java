5ca5307c4ff49772055dbd3e17a36afa471d8300
alumnog <alumnog@S921-13.itson.edu.mx>
2025-04-01 10:27:43 -0700

    Se creo el metodo mango

---
+++ src/main/java/mx/itson/mango/entidades/DispositivoElectronico.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.mango.entidades;
+
+/**
+ *
+ * @author alumnog
+ */
+public class DispositivoElectronico {
+    
+    private String marca;
+    private String modelo;
+    private String color;
+    
+    public DispositivoElectronico(String marca, String modelo, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+    }
+    
+    public String encender() {
+        System.out.println("El dispositivo electronico esta encendido");
+        return null;
+    
+    }
+    
+}
